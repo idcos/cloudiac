b4097a23752303d6a9afc35ddcddf5d4174ee84c
jinxing <jinxing@idcos.com>
2021-06-10 22:33:03 +0800

    feat: 增加 log.log_path 配置项
    用于指定日志文件保存路径，默认日志仅打印到 stdout

--- utils/mail/mail_test.go
+++ utils/mail/mail_test.go
@@ -8,7 +8,7 @@ import (
 
 func TestSendMail(t *testing.T) {
 	configs.Init("../../config.yml")
-	logs.Init("debug",7,"1")
+	logs.Init("debug","1", 1)
 
 	err := SendMail([]string{"13624015331@163.com"}, "测试", "<h1>测试</h1>")
 	if err != nil {
