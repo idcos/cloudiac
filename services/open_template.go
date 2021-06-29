194bb9e951a1180964f2e9b3f4e0866643998dc8
xiaohei <yewei.du@bespinglobal.cn>
2021-06-29 21:56:34 +0800

    feat: open-api模板查询接口增加查询参数

--- services/open_template.go
+++ services/open_template.go
@@ -3,11 +3,16 @@ package services
 import (
 	"cloudiac/libs/db"
 	"cloudiac/models"
+	"fmt"
 )
 
-func OpenSearchTemplate(tx *db.Session) *db.Session {
-	return tx.Table(models.Template{}.TableName()).
+func OpenSearchTemplate(tx *db.Session, q string) *db.Session {
+	query := tx.Table(models.Template{}.TableName()).
 		Where("status = 'enable'").
 		Where("save_state = 0").
 		LazySelectAppend("guid", "name", "tpl_type", "description")
+	if q != "" {
+		query = query.Where("name like ?", fmt.Sprintf("%%%s%%", q))
+	}
+	return query
 }
