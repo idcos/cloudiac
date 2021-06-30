3e42ef760a3ab743c0eba5820233a7493dd87be1
jinxing <jinxing@idcos.com>
2021-06-30 17:35:14 +0800

    feat: 生成 ssh key pair 时自动创建目录

--- portal/services/sshkey/sshkey.go
+++ portal/services/sshkey/sshkey.go
@@ -11,18 +11,22 @@ import (
 	"github.com/pkg/errors"
 	"golang.org/x/crypto/ssh"
 	"os"
+	"path/filepath"
 )
 
 func generateSSHKeyPair(privatePath string, publicPath string) (err error) {
 	openFile := func(p string) (*os.File, error) {
-		if _, err := os.Stat(p); err != nil {
-			if !os.IsNotExist(err) {
-				return nil, err
-			}
-		} else {
-			return nil, errors.Wrap(os.ErrExist, p)
+		if err := os.MkdirAll(filepath.Dir(p), 0755); err != nil {
+			return nil, err
 		}
-		return os.OpenFile(p, os.O_WRONLY|os.O_CREATE, 0600)
+
+		if _, err := os.Stat(p); err == nil {
+			return nil, errors.Wrap(os.ErrExist, p)
+		} else if os.IsNotExist(err) {
+			return os.OpenFile(p, os.O_WRONLY|os.O_CREATE, 0600)
+		} else {
+			return nil, err
+		}
 	}
 
 	privateKey, err := rsa.GenerateKey(rand.Reader, 4096)
