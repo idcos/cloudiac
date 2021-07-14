1a3935668c740ece74a1e89d065252bae83224be
jinxing <jinxing@idcos.com>
2021-07-14 20:03:03 +0800

    feat: 同一环境下的任务按创建时间排队执行

--- portal/task_manager/manager.go
+++ portal/task_manager/manager.go
@@ -36,8 +36,8 @@ type TaskManager struct {
 	db     *db.Session
 	logger logs.Logger
 
-	tasks       map[string]*models.Task
-	runnerTasks map[string]int // 每个 runner 正在执行的任务数量
+	envRunningTask sync.Map       // 每个环境下正在执行的任务
+	runnerTaskNum  map[string]int // 每个 runner 正在执行的任务数量
 
 	wg sync.WaitGroup // 等待执行任务协程退出的 wait group
 
@@ -74,8 +74,8 @@ func Start(serviceId string) {
 
 func (m *TaskManager) reset() {
 	m.db = db.Get()
-	m.tasks = make(map[string]*models.Task)
-	m.runnerTasks = make(map[string]int)
+	m.envRunningTask = sync.Map{}
+	m.runnerTaskNum = make(map[string]int)
 	m.wg = sync.WaitGroup{}
 
 	m.taskStartingCh = make(chan *models.Task)
@@ -146,7 +146,7 @@ func (m *TaskManager) start() {
 	}
 
 	// 查询待运行任务列表的间隔
-	ticker := time.NewTicker(time.Second)
+	ticker := time.NewTicker(time.Second * 5)
 	defer ticker.Stop()
 
 	for {
@@ -169,11 +169,11 @@ func (m *TaskManager) recoverTask(ctx context.Context) error {
 
 	tasks := make([]*models.Task, 0)
 	if err := query.Find(&tasks); err != nil {
-		logger.Errorf("find %s tasks error: %v", models.TaskRunning, err)
+		logger.Errorf("find '%s' tasks error: %v", models.TaskRunning, err)
 		return err
 	}
 
-	logger.Infof("find %d running tasks", len(tasks))
+	logger.Infof("find '%d' running tasks", len(tasks))
 	go func() {
 		for _, task := range tasks {
 			select {
@@ -193,26 +193,38 @@ func (m *TaskManager) run(ctx context.Context) {
 	logger := m.logger
 
 	limitedRunners := make([]string, 0)
-	for runnerId, count := range m.runnerTasks {
+	for runnerId, count := range m.runnerTaskNum {
 		if count >= m.maxTasksPerRunner {
 			limitedRunners = append(limitedRunners, runnerId)
 		}
 	}
 
-	queryTaskLimit := 64 // 单次查询任务数量限制
-	query := m.db.Model(&models.Task{}).
-		Where("status = ?", models.TaskPending).
-		Order("id").
-		Limit(queryTaskLimit)
+	runningEnvs := make([]models.Id, 0)
+	m.envRunningTask.Range(func(key, value interface{}) bool {
+		runningEnvs = append(runningEnvs, key.(models.Id))
+		return true
+	})
 
-	// 查询时过滤掉己达并发限制的 runner
-	if len(limitedRunners) > 0 {
-		query = query.Where("ct_service_id NOT IN (?)", limitedRunners)
+	selectTable := fmt.Sprintf("iac_task, "+
+		"(SELECT env_id, min(created_at) AS created_at FROM iac_task "+
+		"WHERE status = '%s' GROUP BY env_id) AS min_task", models.TaskPending)
+	query := m.db.Unscoped().Table(selectTable).Where(
+		"min_task.env_id = iac_task.env_id AND min_task.created_at = iac_task.created_at")
+
+	if len(runningEnvs) > 0 {
+		// 过滤掉同一环境下有其他任务在执行的任务
+		query = query.Where("iac_task.env_id NOT IN (?)", runningEnvs)
 	}
 
+	if len(limitedRunners) > 0 {
+		// 查询时过滤掉己达并发限制的 runner
+		query = query.Where("runner_id NOT IN (?)", limitedRunners)
+	}
+
+	queryTaskLimit := 64 // 单次查询任务数量限制
 	tasks := make([]*models.Task, 0)
-	if err := query.Find(&tasks); err != nil {
-		logger.Panicf("find '%s' tasks error: %v", models.TaskPending, err)
+	if err := query.Limit(queryTaskLimit).Find(&tasks); err != nil {
+		logger.Panicf("find '%s' task error: %v", models.TaskPending, err)
 	}
 
 	for i := range tasks {
@@ -224,15 +236,25 @@ func (m *TaskManager) run(ctx context.Context) {
 
 		task := tasks[i]
 		// 判断 runner 并发数量
-		n := m.runnerTasks[task.RunnerId]
+		n := m.runnerTaskNum[task.RunnerId]
 		if n >= m.maxTasksPerRunner {
 			logger.WithField("count", n).Infof("runner %s: %v", task.RunnerId, ErrMaxTasksPerRunner)
 			continue
 		}
 
+		if v, loaded := m.envRunningTask.LoadOrStore(task.EnvId, task); loaded {
+			t := v.(*models.Task)
+			logger.Infof("environment '%s' has running task '%s', wait it", task.EnvId, t.Id)
+			continue
+		}
+
 		m.wg.Add(1)
 		go func() {
-			defer m.wg.Done()
+			defer func() {
+				m.envRunningTask.Delete(task.EnvId)
+				m.wg.Done()
+			}()
+
 			m.runTask(ctx, task)
 			if task.IsEffectTask() {
 				// 不管任务成功还是失败都执行
@@ -244,7 +266,6 @@ func (m *TaskManager) run(ctx context.Context) {
 
 func (m *TaskManager) runTask(ctx context.Context, task *models.Task) {
 	logger := m.logger.WithField("taskId", task.Id)
-	logger.Infof("run task: %s", task.Id)
 
 	changeTaskStatus := func(status, message string) error {
 		if er := services.ChangeTaskStatus(m.db, task, status, message); er != nil {
@@ -259,6 +280,7 @@ func (m *TaskManager) runTask(ctx context.Context, task *models.Task) {
 		_ = changeTaskStatus(models.TaskFailed, err.Error())
 	}
 
+	logger.Infof("run task: %s", task.Id)
 	// 先更新任务为 running 状态
 	// 极端情况下任务未执行好过重复执行，所以先设置状态，后发起调用
 	if err := changeTaskStatus(models.TaskRunning, ""); err != nil {
