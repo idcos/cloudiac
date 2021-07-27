671b076021442e65cf0adf66e0c9239468d688f7
xiaohei <duyewei@idcos.com>
2021-07-27 15:21:18 +0800

    change: 修改用户邀请邮件模板样式

--- portal/consts/notification_template.go
+++ portal/consts/notification_template.go
@@ -3,31 +3,24 @@ package consts
 var IacUserInvitationsTpl = `
 <html>
 <body>
-<span>尊敬的 {{.Name}}：</span>
-<br />
-<br />
-<span>&nbsp;&nbsp;&nbsp;&nbsp;CloudIaC的管理员 【{{.Inviter}}】 邀请您体验CloudIaC平台并加入 【{{.Organization}}】组织，您可以通过以下方式登录CloudIaC平台：</span>
-<br />
-<br />
-<span>&nbsp;&nbsp;&nbsp;&nbsp;平台地址：<a href="{{.Addr}}">{{.Addr}}</a></span>
-<br />
-<span>&nbsp;&nbsp;&nbsp;&nbsp;帐户：{{.Email}}</span>
-<br />
+尊敬的 {{.Name}}：
+<br>
+<br>&nbsp;&nbsp;&nbsp;&nbsp;CloudIaC的管理员 【{{.Inviter}}】 邀请您体验CloudIaC平台并加入 【{{.Organization}}】组织，您可以通过以下方式登录CloudIaC平台：
+<br>
+<br>&nbsp;&nbsp;&nbsp;&nbsp;平台地址：<a href="{{.Addr}}">{{.Addr}}</a>
+<br>&nbsp;&nbsp;&nbsp;&nbsp;帐户：{{.Email}}
 {{if .IsNewUser}}
-<span>&nbsp;&nbsp;&nbsp;&nbsp;初始密码：{{.InitPass}}</span>
-<br />
+<br>&nbsp;&nbsp;&nbsp;&nbsp;初始密码：{{.InitPass}}
 {{end}}
+<br>
 {{if .IsNewUser}}
-<br />
-<span>&nbsp;&nbsp;&nbsp;&nbsp;为保障您的帐户安全，请立即登录平台并及时修改初始密码，祝您使用愉快！</span>
-<br />
+<br>&nbsp;&nbsp;&nbsp;&nbsp;为保障您的帐户安全，请立即登录平台并及时修改初始密码，祝您使用愉快！
+<br>
 {{else}}
-<br />
-<span>&nbsp;&nbsp;&nbsp;&nbsp;请使用您的帐户登陆 CloudIaC 平台使用服务，祝您使用愉快！</span>
-<br />
+<br>&nbsp;&nbsp;&nbsp;&nbsp;请使用您的帐户登陆 CloudIaC 平台使用服务，祝您使用愉快！
+<br>
 {{end}}
-<br />
-<span>	-----该邮件由系统自动发出，请勿回复-----</span>
+<br>-----该邮件由系统自动发出，请勿回复-----
 </body>
 </html>
 `
