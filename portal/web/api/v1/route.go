ee639c91719139a1a93e2684b6122d7634141692
xiaohei <duyewei@idcos.com>
2021-11-10 11:34:30 +0800

    del: 删除swagger-host

--- portal/web/api/v1/route.go
+++ portal/web/api/v1/route.go
@@ -17,7 +17,6 @@ import (
 // @description CloudIaC 将易于使用的界面与强大的治理工具相结合，让您和您团队的成员可以快速轻松的在云中部署和管理环境。
 // @description 通过将 CloudIaC 集成到您的流程中，您可以获得对组织的云使用情况的可见性、可预测性和更好的治理。
 
-// @host localhost:9030
 // @BasePath /api/v1
 // @schemes http
 
