d65f3cb896916f1e58e78c830b965604d5648145
jinxing <jxinging@gmail.com>
2021-11-18 21:51:18 +0800

    feat: 创建和更新模板接口的 variables 参数改为全量传入的方式

--- portal/models/forms/template.go
+++ portal/models/forms/template.go
@@ -16,22 +16,23 @@ type Var struct {
 type CreateTemplateForm struct {
 	BaseForm
 
-	Name              string      `form:"name" json:"name" binding:"required,gte=2,lte=64"`
-	Description       string      `form:"description" json:"description" binding:""`
-	RepoId            string      `form:"repoId" json:"repoId" binding:"required"`
-	RepoRevision      string      `form:"repoRevision" json:"repoRevision" binding:""`
-	Extra             string      `form:"extra" json:"extra"`
-	Workdir           string      `form:"workdir" json:"workdir"`
-	VcsId             models.Id   `form:"vcsId" json:"vcsId" binding:"required"`
-	Playbook          string      `json:"playbook" form:"playbook"`
-	PlayVarsFile      string      `json:"playVarsFile" form:"playVarsFile"`
-	TfVarsFile        string      `form:"tfVarsFile" json:"tfVarsFile"`
-	Variables         []Variable `json:"variables" form:"variables" `
-	DeleteVariablesId []string    `json:"deleteVariablesId" form:"deleteVariablesId" ` //变量id
-	ProjectId         []models.Id `form:"projectId" json:"projectId"`                  // 项目ID
-	TfVersion         string      `form:"tfVersion" json:"tfVersion"`                  // 模版使用terraform版本号
-	VarGroupIds       []models.Id `json:"varGroupIds" form:"varGroupIds" `
-	DelVarGroupIds    []models.Id `json:"delVarGroupIds" form:"delVarGroupIds" `
+	Name         string      `form:"name" json:"name" binding:"required,gte=2,lte=64"`
+	Description  string      `form:"description" json:"description" binding:""`
+	RepoId       string      `form:"repoId" json:"repoId" binding:"required"`
+	RepoRevision string      `form:"repoRevision" json:"repoRevision" binding:""`
+	Extra        string      `form:"extra" json:"extra"`
+	Workdir      string      `form:"workdir" json:"workdir"`
+	VcsId        models.Id   `form:"vcsId" json:"vcsId" binding:"required"`
+	Playbook     string      `json:"playbook" form:"playbook"`
+	PlayVarsFile string      `json:"playVarsFile" form:"playVarsFile"`
+	TfVarsFile   string      `form:"tfVarsFile" json:"tfVarsFile"`
+	ProjectId    []models.Id `form:"projectId" json:"projectId"` // 项目ID
+	TfVersion    string      `form:"tfVersion" json:"tfVersion"` // 模版使用terraform版本号
+
+	Variables []Variable `json:"variables" form:"variables" `
+
+	VarGroupIds    []models.Id `json:"varGroupIds" form:"varGroupIds" `
+	DelVarGroupIds []models.Id `json:"delVarGroupIds" form:"delVarGroupIds" `
 }
 
 type SearchTemplateForm struct {
@@ -43,24 +44,25 @@ type SearchTemplateForm struct {
 
 type UpdateTemplateForm struct {
 	BaseForm
-	Id                models.Id   `uri:"id" form:"id" json:"id" binding:"required"`
-	Name              string      `form:"name" json:"name"`
-	Description       string      `form:"description" json:"description"`
-	Status            string      `form:"status" json:"status"`
-	Workdir           string      `form:"workdir" json:"workdir"`
-	RunnerId          string      `json:"runnerId" form:"runnerId"`
-	Playbook          string      `json:"playbook" form:"playbook"`
-	PlayVarsFile      string      `json:"playVarsFile" form:"playVarsFile"`
-	TfVarsFile        string      `form:"tfVarsFile" json:"tfVarsFile"`
-	Variables         []Variable `json:"variables" form:"variables" `
-	DeleteVariablesId []string    `json:"deleteVariablesId" form:"deleteVariablesId" ` //变量id
-	ProjectId         []models.Id `form:"projectId" json:"projectId"`
-	RepoRevision      string      `form:"repoRevision" json:"repoRevision" binding:""`
-	VcsId             models.Id   `form:"vcsId" json:"vcsId" binding:""`
-	RepoId            string      `form:"repoId" json:"repoId" binding:""`
-	TfVersion         string      `form:"tfVersion" json:"tfVersion" binding:""`
-	VarGroupIds       []models.Id `json:"varGroupIds" form:"varGroupIds" `
-	DelVarGroupIds    []models.Id `json:"delVarGroupIds" form:"delVarGroupIds" `
+	Id           models.Id   `uri:"id" form:"id" json:"id" binding:"required"`
+	Name         string      `form:"name" json:"name"`
+	Description  string      `form:"description" json:"description"`
+	Status       string      `form:"status" json:"status"`
+	Workdir      string      `form:"workdir" json:"workdir"`
+	RunnerId     string      `json:"runnerId" form:"runnerId"`
+	Playbook     string      `json:"playbook" form:"playbook"`
+	PlayVarsFile string      `json:"playVarsFile" form:"playVarsFile"`
+	TfVarsFile   string      `form:"tfVarsFile" json:"tfVarsFile"`
+	ProjectId    []models.Id `form:"projectId" json:"projectId"`
+	RepoRevision string      `form:"repoRevision" json:"repoRevision" binding:""`
+	VcsId        models.Id   `form:"vcsId" json:"vcsId" binding:""`
+	RepoId       string      `form:"repoId" json:"repoId" binding:""`
+	TfVersion    string      `form:"tfVersion" json:"tfVersion" binding:""`
+
+	Variables []Variable `json:"variables" form:"variables" `
+
+	VarGroupIds    []models.Id `json:"varGroupIds" form:"varGroupIds" `
+	DelVarGroupIds []models.Id `json:"delVarGroupIds" form:"delVarGroupIds" `
 }
 
 type DeleteTemplateForm struct {
