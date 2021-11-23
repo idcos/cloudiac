1a9ef47de28515808581a445c3adbdbd056b6f26
xiaohei <duyewei@idcos.com>
2021-11-23 15:41:47 +0800

    add: 测试用例

--- portal/services/vcsrv/gitea_test.go
+++ portal/services/vcsrv/gitea_test.go
@@ -22,3 +22,18 @@ func Test_giteaRepoIface_AddWebhook(t *testing.T) {
 
 
 }
+
+func Test_giteaRepoIface_ListBranches(t *testing.T) {
+	gitea := &giteaRepoIface{
+		vcs:          &models.Vcs{
+			Address:   "http://10.0.3.124:3000",
+			VcsToken:  "27b9b370eb32bcbe32200ac37497c0f3a5de10a3",
+		},
+		repository:   &Repository{
+			FullName:      "gitea/cloudiac-example",
+		},
+	}
+	got, err := gitea.ListBranches()
+	fmt.Println(err)
+	fmt.Println(got)
+}
