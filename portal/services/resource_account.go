22bfe3a7c49adc7d4eb770fbe1ee779aa4da582f
xiaohei <duyewei@idcos.com>
2021-11-25 15:41:12 +0800

    fix: 查询资源详情未指定id

--- portal/services/resource_account.go
+++ portal/services/resource_account.go
@@ -110,12 +110,12 @@ func GetResourceById(tx *db.Session, id models.Id) (*models.Resource, e.Error) {
 	return &r, nil
 }
 
-func GetResourceByEnv(tx *db.Session, env *models.Env) (*Resource, e.Error) {
+func GetResourceDetail(tx *db.Session, env *models.Env, resourceId models.Id) (*Resource, e.Error) {
 	r := &Resource{}
-	if err := tx.Table("iac_resource as r").
-		Joins("left join iac_resource_drift as rd on rd.address =  r.address  and rd.env_id = ? and rd.task_id", env.Id, env.LastCronTaskId).
-		Where("r.org_id = ? AND r.project_id = ? AND r.env_id = ? AND r.task_id = ?",
-			env.OrgId, env.ProjectId, env.Id, env.LastResTaskId).
+	if err := tx.Table("iac_resource as r").Debug().
+		Joins("left join iac_resource_drift as rd on rd.address = r.address  and rd.env_id = ? and rd.task_id = ?", env.Id, env.LastCronTaskId).
+		Where("r.org_id = ? AND r.project_id = ? AND r.env_id = ? AND r.task_id = ? AND r.id = ?",
+			env.OrgId, env.ProjectId, env.Id, env.LastResTaskId, resourceId).
 		LazySelectAppend("r.*, rd.resource_detail, rd.create_at").
 		First(r); err != nil {
 		return nil, e.New(e.DBError, err)
