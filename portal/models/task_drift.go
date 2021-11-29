54fbd57489d18499f81894855bed18d19f2c7d30
xuxiaoahang2018 <824452323@qq.com>
2021-11-29 17:55:59 +0800

    修改偏移检测信息表结构

--- portal/models/task_drift.go
+++ portal/models/task_drift.go
@@ -4,6 +4,9 @@ type ResourceDrift struct {
 	AutoUintIdModel
 	ResId       Id     `json:"resId" gorm:"size:32;not null"`
 	DriftDetail string `json:"driftDetail" gorm:"type:text"`
+	CreateAt *Time `json:"createAt" gorm:"type:datetime;comment:任务开始时间"` // 任务开始时间
+	UpdateAt *Time `json:"updateAt" gorm:"type:datetime;comment:任务开始时间"` // 任务开始时间
+
 }
 
 func (ResourceDrift) TableName() string {
