17977f6aee30bcf11aa625c64f37aac774ed7993
xuxiaoahang2018 <824452323@qq.com>
2021-11-29 18:39:30 +0800

    修复偏移检测表model

--- portal/models/task_drift.go
+++ portal/models/task_drift.go
@@ -1,11 +1,9 @@
 package models
 
 type ResourceDrift struct {
-	AutoUintIdModel
+	TimedModel
 	ResId       Id     `json:"resId" gorm:"size:32;not null"`
 	DriftDetail string `json:"driftDetail" gorm:"type:text"`
-	CreateAt *Time `json:"createAt" gorm:"type:datetime;comment:任务开始时间"` // 任务开始时间
-	UpdateAt *Time `json:"updateAt" gorm:"type:datetime;comment:任务开始时间"` // 任务开始时间
 }
 
 func (ResourceDrift) TableName() string {
