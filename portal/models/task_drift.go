1e15046fdc6e709b192099ed5b5d84fd82071839
xuxiaoahang2018 <824452323@qq.com>
2021-11-29 14:18:39 +0800

    修复漂移资源数据表结构

--- portal/models/task_drift.go
+++ portal/models/task_drift.go
@@ -2,11 +2,8 @@ package models
 
 type ResourceDrift struct {
 	AutoUintIdModel
-	EnvId          Id     `json:"envId" gorm:"size:32;not null"`
-	CreateAt       *Time  `json:"createAt" gorm:"type:datetime"`
-	TaskId         Id     `json:"taskId" gorm:"size:32;not null"`
-	Address        string `json:"address" gorm:"size:255;not null"`
-	ResourceDetail string `json:"resourceDetail" gorm:"type:text"`
+	ResId       Id     `json:"resId" gorm:"size:32;not null"`
+	DriftDetail string `json:"driftDetail" gorm:"type:text"`
 }
 
 func (ResourceDrift) TableName() string {
