b7ebe3c218d218feddf19bd56cf921096f240993
jinxing <jxinging@gmail.com>
2021-12-07 19:51:04 +0800

    feat: 自动销毁任务总是使用环境最后一次部署时的 commit

--- portal/services/task_auto_destory.go
+++ portal/services/task_auto_destory.go
@@ -22,18 +22,7 @@ func CreateAutoDestroyTask(tx *db.Session, env *models.Env) (*models.Task, e.Err
 		return nil, err
 	}
 
-	pipeline := ""
-	if env.LastResTaskId != "" {
-		// 自动销毁任务使用环境最后一次部署时的 pipeline
-		lastResTask, err := GetTaskById(tx, env.LastResTaskId)
-		if err != nil {
-			logger.Errorf("get env lastResTask error: %v", err)
-			return nil, err
-		}
-		pipeline = lastResTask.Pipeline
-	}
-
-	return CreateTask(tx, tpl, env, models.Task{
+	paramTask := models.Task{
 		Name:            "Auto Destroy",
 		Targets:         nil,
 		CreatorId:       consts.SysUserId,
@@ -41,10 +30,20 @@ func CreateAutoDestroyTask(tx *db.Session, env *models.Env) (*models.Task, e.Err
 		AutoApprove:     true,
 		StopOnViolation: env.StopOnViolation,
 		BaseTask: models.BaseTask{
-			Type:        models.TaskTypeDestroy,
-			Pipeline:    pipeline,
-			StepTimeout: 0,
-			RunnerId:    "",
+			Type: models.TaskTypeDestroy,
 		},
-	})
+	}
+
+	if env.LastResTaskId != "" {
+		// 自动销毁任务使用环境最后一次部署时的 pipeline
+		lastResTask, err := GetTaskById(tx, env.LastResTaskId)
+		if err != nil {
+			logger.Errorf("get env lastResTask error: %v", err)
+			return nil, err
+		}
+		paramTask.Pipeline = lastResTask.Pipeline
+		paramTask.CommitId = lastResTask.CommitId
+	}
+
+	return CreateTask(tx, tpl, env, paramTask)
 }
