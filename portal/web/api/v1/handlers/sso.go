cccc0e0f40b0975c7b1f3278358944cb6201ed0c
wangyubin <linuxr@aliyun.com>
2021-12-27 13:22:21 +0800

    add swagger doc for sso api

--- portal/web/api/v1/handlers/sso.go
+++ portal/web/api/v1/handlers/sso.go
@@ -6,14 +6,27 @@ import (
 	"cloudiac/portal/models/forms"
 )
 
-// TODO: swagger docs
 // GenerateSsoToken 生成 SSO token
+// @Summary 生成 token 供单点登录使用
+// @Tags SSO
+// @Accept  json
+// @Produce  json
+// @Security AuthToken
+// @Success 200 {object}  ctx.JSONResult{result=models.SsoResp}
+// @Router /sso/tokens [post]
 func GenerateSsoToken(c *ctx.GinRequest) {
 	c.JSONResult(apps.GenerateSsoToken(c.Service()))
 }
 
-// TODO: swagger docs
 // VerifySsoToken 验证 SSO token
+// @Summary 验证单点登录的 token，返回单点登录的用户信息
+// @Tags SSO
+// @Accept  json
+// @Produce  json
+// @Security AuthToken
+// @Param token query string true "sso token"
+// @Success 200 {object}  ctx.JSONResult{result=models.VerifySsoTokenResp}
+// @Router /sso/tokens [post]
 func VerifySsoToken(c *ctx.GinRequest) {
 	form := forms.VerifySsoTokenForm{}
 	if err := c.Bind(&form); err != nil {
