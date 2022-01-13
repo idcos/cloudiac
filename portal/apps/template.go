c94bf4b4c6af7017f3dd7737e3cacf7ac25110ca
xuxiaoahang2018 <824452323@qq.com>
2022-01-13 18:58:21 +0800

    更新 活跃环境以及关联环境

--- portal/apps/template.go
+++ portal/apps/template.go
@@ -20,23 +20,24 @@ import (
 )
 
 type SearchTemplateResp struct {
-	CreatedAt         models.Time `json:"createdAt"` // 创建时间
-	UpdatedAt         models.Time `json:"updatedAt"` // 更新时间
-	Id                models.Id   `json:"id"`
-	Name              string      `json:"name"`
-	Description       string      `json:"description"`
-	ActiveEnvironment int         `json:"activeEnvironment"`
-	RepoRevision      string      `json:"repoRevision"`
-	Creator           string      `json:"creator"`
-	RepoId            string      `json:"repoId"`
-	VcsId             string      `json:"vcsId"`
-	RepoAddr          string      `json:"repoAddr"`
-	TplType           string      `json:"tplType" `
-	RepoFullName      string      `json:"repoFullName"`
-	NewRepoAddr       string      `json:"newRepoAddr"`
-	VcsAddr           string      `json:"vcsAddr"`
-	PolicyEnable      bool        `json:"policyEnable"`
-	PolicyStatus      string      `json:"policyStatus"`
+	CreatedAt           models.Time `json:"createdAt"` // 创建时间
+	UpdatedAt           models.Time `json:"updatedAt"` // 更新时间
+	Id                  models.Id   `json:"id"`
+	Name                string      `json:"name"`
+	Description         string      `json:"description"`
+	ActiveEnvironment   int         `json:"activeEnvironment"`
+	RelationEnvironment int         `json:"relationEnvironment"`
+	RepoRevision        string      `json:"repoRevision"`
+	Creator             string      `json:"creator"`
+	RepoId              string      `json:"repoId"`
+	VcsId               string      `json:"vcsId"`
+	RepoAddr            string      `json:"repoAddr"`
+	TplType             string      `json:"tplType" `
+	RepoFullName        string      `json:"repoFullName"`
+	NewRepoAddr         string      `json:"newRepoAddr"`
+	VcsAddr             string      `json:"vcsAddr"`
+	PolicyEnable        bool        `json:"policyEnable"`
+	PolicyStatus        string      `json:"policyStatus"`
 }
 
 func getRepoAddr(vcsId models.Id, query *db.Session, repoId string) (string, error) {
