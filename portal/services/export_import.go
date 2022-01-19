63533cd8d4a40dd978e1b41bed371204e371b1e2
jinxing <jxinging@gmail.com>
2022-01-19 14:41:36 +0800

    fix: 修复导出云模板时敏感变量处理问题
    如果导出时敏感变量没有 secret: 前缀会导致没有先解密就被导出

--- portal/services/export_import.go
+++ portal/services/export_import.go
@@ -3,6 +3,7 @@ package services
 import (
 	"cloudiac/configs"
 	"cloudiac/utils"
+	"cloudiac/utils/logs"
 )
 
 // 对传入的字符串使用 exportSecretKey 加密，并添加加密前缀标识
@@ -51,11 +52,15 @@ func ExportVariableValue(val string, senstive bool) string {
 	if val == "" {
 		return val
 	}
-
 	if !senstive {
 		return val
 	}
-	val, _ = utils.DecryptSecretVar(val)
+
+	var err error
+	val, err = utils.DecryptSecretVarForce(val)
+	if err != nil {
+		logs.Get().Panicln(err)
+	}
 	return ExportSecretStr(val, false)
 }
 
