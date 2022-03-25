98514d0862d3ad058f15b1cf3f416584e6462f10
cong2960 <88026860+cong2960@users.noreply.github.com>
2022-03-25 19:18:08 +0800

    Feat form checks (#416)
    * feat-form-checks
    
    * 增加form字段校验功能
    
    * Merge branch 'dev0.11' into feat-form-checks
    
    # Conflicts:
    #	portal/models/forms/env.go
    
    * 修复一些校验错误
    
    * 改进一些校验规则
    
    Co-authored-by: lilecong <lilecong@idcos.com>

--- portal/models/forms/resource_account.go
+++ portal/models/forms/resource_account.go
@@ -13,27 +13,26 @@ type Params struct {
 
 type CreateResourceAccountForm struct {
 	BaseForm
-	Name         string   `form:"name" json:"name" binding:"required,gte=2,lte=32"` // 资源账号名字
-	Description  string   `form:"description" json:"description"`
-	Params       []Params `form:"params" json:"params"`
+	Name         string   `form:"name" json:"name" binding:"required,gte=2,lte=32"`
+	Description  string   `form:"description" json:"description" binding:"max=255"`
+	Params       []Params `form:"params" json:"params" binding:"required"`
 	CtServiceIds []string `form:"ctServiceIds" json:"ctServiceIds"`
 }
 
 type UpdateResourceAccountForm struct {
 	BaseForm
-	Id           models.Id `uri:"id" json:"id" swaggerignore:"true" binding:"required"`
-	Name         string    `form:"name" json:"name" binding:""`
-	Description  string    `form:"description" json:"description"`
+	Id           models.Id `form:"id" json:"id" binding:"required" swaggerignore:"true"`
+	Name         string    `form:"name" json:"name" binding:"omitempty,gte=2,lte=32"`
+	Description  string    `form:"description" json:"description" binding:"max=255"`
 	Params       []Params  `form:"params" json:"params"`
-	Status       string    `form:"status" json:"status"`
+	Status       string    `form:"status" json:"status" binding:"omitempty,oneof=enable,disable"`
 	CtServiceIds []string  `form:"ctServiceIds" json:"ctServiceIds"`
 }
 
 type SearchResourceAccountForm struct {
 	PageForm
-
-	Q      string `form:"q" json:"q" binding:""`                       // 资源账号名或者描述 支持模糊查询
-	Status string `form:"status" json:"status" enums:"enable,disable"` // 资源账号状态
+	Q      string `form:"q" json:"q" binding:""`                                         // 资源账号名或者描述 支持模糊查询
+	Status string `form:"status" json:"status" binding:"omitempty,oneof=enable disable"` // 资源账号状态
 }
 
 type DeleteResourceAccountForm struct {
