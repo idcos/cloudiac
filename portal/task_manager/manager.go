f1b467e55ad5991a7986f9e224e0a98131f577fc
lilecong <lilecong@idcos.com>
2022-03-30 16:23:45 +0800

    将任务结束才更新资源变更数据改为执行部署的plan以后就更新资源变更数据

--- portal/task_manager/manager.go
+++ portal/task_manager/manager.go
@@ -673,11 +673,11 @@ func (m *TaskManager) processTaskDone(taskId models.Id) { //nolint:cyclop
 
 		// 任务执行成功才会进行 changes 统计，失败的话基于 plan 文件进行变更统计是不准确的
 		// (terraform 执行 apply 失败也不会输出资源变更情况)
-		if lastStep.Status == models.TaskComplete {
+		/*if lastStep.Status == models.TaskComplete {
 			if err := taskDoneProcessPlan(dbSess, task); err != nil {
 				logger.Errorf("process task plan: %v", err)
 			}
-		}
+		}*/
 	}
 
 	if lastStep.Status == models.TaskComplete && task.IsDriftTask {
@@ -792,6 +792,10 @@ func waitTaskStepApprove(ctx context.Context, db *db.Session, task *models.Task,
 	if step.MustApproval && !step.IsApproved() {
 		logger.Infof("waitting task step approve")
 		changeStepStatus(models.TaskStepApproving, "", step)
+		err = taskDoneProcessPlan(db, task)
+		if err != nil {
+			logger.Errorf("process task plan: %v", err)
+		}
 		if newStep, err = WaitTaskStepApprove(ctx, db, step.TaskId, step.Index); err != nil {
 			if errors.Is(err, context.Canceled) {
 				return nil, err
