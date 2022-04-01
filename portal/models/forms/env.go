65949a939d5ac799df4024b0338dc7f1f78620b1
cong2960 <88026860+cong2960@users.noreply.github.com>
2022-04-01 23:39:03 +0800

    Feat: 环境重新部署增加extraData参数 (#461)

--- portal/models/forms/env.go
+++ portal/models/forms/env.go
@@ -120,9 +120,11 @@ type DeployEnvForm struct {
 	Revision   string   `form:"revision" json:"revision" binding:""`                                    // 分支/标签
 	Timeout    int      `form:"timeout" json:"timeout" binding:""`                                      // 部署超时时间（单位：秒）
 
-	RetryNumber int  `form:"retryNumber" json:"retryNumber" binding:""` // 重试总次数
-	RetryDelay  int  `form:"retryDelay" json:"retryDelay" binding:""`   // 重试时间间隔
-	RetryAble   bool `form:"retryAble" json:"retryAble" binding:""`     // 是否允许任务进行重试
+	RetryNumber int         `form:"retryNumber" json:"retryNumber" binding:""` // 重试总次数
+	RetryDelay  int         `form:"retryDelay" json:"retryDelay" binding:""`   // 重试时间间隔
+	RetryAble   bool        `form:"retryAble" json:"retryAble" binding:""`     // 是否允许任务进行重试
+
+	ExtraData   models.JSON `form:"extraData" json:"extraData" binding:""`     // 扩展字段，用于存储外部服务调用时的信息
 
 	Variables []Variable `form:"variables" json:"variables" binding:""` // 自定义变量列表，该变量列表会覆盖现有的变量
 
