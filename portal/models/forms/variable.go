882d4544cb7734ff88fe04d1e5ea2aa6973c06f0
jinxing <jxinging@gmail.com>
2022-04-07 00:19:16 +0800

    fix: 创建环境时变量 id 为空会报错
    新创建的变量其 id 是空，不应该检查 startswith

--- portal/models/forms/variable.go
+++ portal/models/forms/variable.go
@@ -22,7 +22,7 @@ type UpdateObjectVarsForm struct {
 }
 
 type Variable struct {
-	Id          models.Id       `json:"id" form:"id" binding:"omitempty,startswith=var-,max=32"`
+	Id          models.Id       `json:"id" form:"id" binding:"omitempty,max=32"`
 	Scope       string          `json:"scope" form:"scope" binding:"omitempty,oneof=org template project env"`    // 应用范围 ('org','template','project','env')
 	Type        string          `json:"type" form:"type" binding:"omitempty,oneof=environment terraform ansible"` // 类型 ('environment','terraform','ansible')
 	Name        string          `json:"name" form:"name" binding:"required,max=64"`                               // 名称
