3189dfc33ed3d1e71f05f352b0c643da2bf7ffc7
xiaohei <41576095+xiaohei417@users.noreply.github.com>
2022-04-06 23:04:49 +0800

    feat: kafka消息增加任务类型及环境状态 (#467)
    * feat: kafka消息增加任务类型及环境状态
    
    * fix: 任务完成后变更环境状态后直接退出了，后续消息通知逻辑未执行
    
    * fix: 环境修改状态异常时，不能进行下一步回调

--- utils/kafka/kafka.go
+++ utils/kafka/kafka.go
@@ -30,6 +30,8 @@ type IacKafkaContent struct {
 	EventType  string                 `json:"eventType"`
 	ExtraData  interface{}            `json:"extraData"`
 	TaskStatus string                 `json:"taskStatus"`
+	TaskType   string                 `json:"taskType"`
+	EnvStatus  string                 `json:"envStatus"`
 	OrgId      models.Id              `json:"orgId"`
 	ProjectId  models.Id              `json:"projectId"`
 	TplId      models.Id              `json:"tplId"`
@@ -37,9 +39,11 @@ type IacKafkaContent struct {
 	Result     IacKafkaCallbackResult `json:"result"`
 }
 
-func (k *KafkaProducer) GenerateKafkaContent(task *models.Task, taskStatus string, resources []models.Resource) []byte {
+func (k *KafkaProducer) GenerateKafkaContent(task *models.Task, taskStatus, envStatus string, resources []models.Resource) []byte {
 	a := IacKafkaContent{
 		TaskStatus: taskStatus,
+		TaskType:   task.Type,
+		EnvStatus:  envStatus,
 		OrgId:      task.OrgId,
 		ProjectId:  task.ProjectId,
 		TplId:      task.TplId,
@@ -49,9 +53,9 @@ func (k *KafkaProducer) GenerateKafkaContent(task *models.Task, taskStatus strin
 		},
 	}
 
-	if task.ExtraData != nil{
+	if task.ExtraData != nil {
 		a.ExtraData = task.ExtraData
-	}else {
+	} else {
 		a.ExtraData = make(map[string]interface{})
 	}
 
