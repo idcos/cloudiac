ba147a6b24e7b012c73caba363eea9b5b85bba7a
wangyubin <linuxr@aliyun.com>
2022-04-21 02:02:09 +0800

    根据项目ID获取资源账号 vargroup

--- portal/services/variables_group.go
+++ portal/services/variables_group.go
@@ -549,3 +549,18 @@ func GetVarGroupIsOpenBillCollectByVgIds(dbSess *db.Session, vgIds []models.Id, 
 
 	return exists
 }
+
+func GetBillVarGroupByProjectId(dbSess *db.Session, projectId models.Id, provider string) (*models.VariableGroup, e.Error) {
+	query := dbSess.Model(&models.VariableGroup{}).Joins(`join iac_variable_group_project_rel on iac_variable_group.id = iac_variable_group_project_rel.var_group_id`)
+
+	query = query.Where("iac_variable_group_project_rel.project_id = ?", projectId)
+	query = query.Where("iac_variable_group.type = ?", "environment")
+	query = query.Where("iac_variable_group.provider = ?", provider)
+
+	var vg models.VariableGroup
+	if err := query.First(&vg); err != nil {
+		return nil, e.AutoNew(err, e.DBError)
+	}
+
+	return &vg, nil
+}
