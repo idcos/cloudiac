a1bf33fce896e7569e4d05248dda8cd704c7f29e
wangyubin <linuxr@aliyun.com>
2022-04-22 15:23:46 +0800

    预估费用换算成月度费用

--- portal/task_manager/task_done_actions.go
+++ portal/task_manager/task_done_actions.go
@@ -112,7 +112,7 @@ func getForecastCostWhenTaskPlan(dbSess *db.Session, task *models.Task, bs []byt
 		return nil, err
 	}
 
-	return []float32{addedCost, destroyedCost, updateAfterCost - updateBeforeCost}, err
+	return []float32{addedCost * 730, destroyedCost * 730, (updateAfterCost - updateBeforeCost) * 730}, err
 }
 
 func getPriceService(dbSess *db.Session, projectId models.Id, provider string) (pricecalculator.PriceService, error) {
