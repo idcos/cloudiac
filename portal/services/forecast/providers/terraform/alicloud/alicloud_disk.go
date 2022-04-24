dbe535c86199dacb3fa2dc64a530cda5be1de51d
xiaohei <duyewei@idcos.com>
2022-04-24 10:08:08 +0800

    fix: sonnar bug

--- portal/services/forecast/providers/terraform/alicloud/alicloud_disk.go
+++ portal/services/forecast/providers/terraform/alicloud/alicloud_disk.go
@@ -2,28 +2,23 @@
 
 package alicloud
 
-import (
-	"cloudiac/portal/services/forecast/reource/alicloud"
-	"cloudiac/portal/services/forecast/schema"
-)
-
-func getDiskRegistryItem() *schema.RegistryItem {
-	return &schema.RegistryItem{
-		Name:  "alicloud_disk",
-		Notes: []string{},
-		RFunc: NewDisk,
-	}
-}
-
-func NewDisk(d *schema.ResourceData) *schema.Resource {
-	region := d.Get("region").String()
-
-	a := &alicloud.Disk{
-		Address:  d.Address,
-		Provider: d.ProviderName,
-		Region:   region,
-	}
-
-	return a.BuildResource()
-
-}
+//func getDiskRegistryItem() *schema.RegistryItem {
+//	return &schema.RegistryItem{
+//		Name:  "alicloud_disk",
+//		Notes: []string{},
+//		RFunc: NewDisk,
+//	}
+//}
+//
+//func NewDisk(d *schema.ResourceData) *schema.Resource {
+//	region := d.Get("region").String()
+//
+//	a := &alicloud.Disk{
+//		Address:  d.Address,
+//		Provider: d.ProviderName,
+//		Region:   region,
+//	}
+//
+//	return a.BuildResource()
+//
+//}
