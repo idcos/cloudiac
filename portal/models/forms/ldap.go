68c3a9cdf9f01cf2b559506b669231562b93cde8
wangyubin <64903517@qq.com>
2022-05-19 15:48:06 +0800

    调整ldap授权接口参数 (#559)
    * 调整组织授权api的参数
    
    * 调整项目OU授权api的参数
    
    Co-authored-by: wangyubin <linuxr@aliyun.com>

--- portal/models/forms/ldap.go
+++ portal/models/forms/ldap.go
@@ -17,16 +17,13 @@ type AuthLdapUserForm struct {
 	BaseForm
 
 	Email string `json:"email" form:"email"` // 邮箱
-	Uid   string `json:"uid" form:"uid"`     // 用户名称
 	Role  string `json:"role" form:"role"`   // 角色
-	Phone string `json:"phone" form:"phone"` // 手机号
 }
 
 type AuthLdapOUForm struct {
 	BaseForm
 
 	DN   string `json:"dn" form:"dn"`     // 识别名
-	OU   string `json:"ou" form:"ou"`     // organizationalUnit
 	Role string `json:"role" form:"role"` // 角色
 }
 
@@ -46,8 +43,6 @@ type UpdateLdapOUForm struct {
 type AuthProjectLdapOUForm struct {
 	BaseForm
 
-	ProjectId string `json:"projectId" form:"projectId"` // project id
-	DN        string `json:"dn" form:"dn"`               // 识别名
-	OU        string `json:"ou" form:"ou"`               // organizationalUnit
-	Role      string `json:"role" form:"role"`           // 角色
+	DN   string `json:"dn" form:"dn"`     // 识别名
+	Role string `json:"role" form:"role"` // 角色
 }
