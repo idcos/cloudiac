7e6cf323d073516a9af6c6f87a77d3d5df8e6c38
wangyubin <linuxr@aliyun.com>
2022-07-06 11:15:40 +0800

    add test sample

--- portal/services/resource_sensitive_test.go
+++ portal/services/resource_sensitive_test.go
@@ -7,7 +7,7 @@ import (
 	"testing"
 )
 
-const tfplanCase = `
+const tfplanCase01 = `
 {
 	"format_version": "0.1",
 	"terraform_version": "0.15.5",
@@ -151,8 +151,8 @@ const tfplanCase = `
 	}
   }`
 
-func TestGetSensitiveKeysFromTfPlan(t *testing.T) {
-	result := GetSensitiveKeysFromTfPlan([]byte(tfplanCase))
+func TestGetSensitiveKeysFromTfPlanCase01(t *testing.T) {
+	result := GetSensitiveKeysFromTfPlan([]byte(tfplanCase01))
 
 	if result == nil {
 		t.Error("解析失败")
@@ -172,7 +172,7 @@ func TestGetSensitiveKeysFromTfPlan(t *testing.T) {
 	}
 }
 
-func TestSensitiveAttrs01(t *testing.T) {
+func TestCase01SensitiveAttrs01(t *testing.T) {
 	// name 是敏感变量
 	var attrs = map[string]interface{}{
 		"name":    "name_value",
@@ -180,7 +180,7 @@ func TestSensitiveAttrs01(t *testing.T) {
 		"address": "module.m.module.m2.docker_container.nginx",
 	}
 
-	sKeys := GetSensitiveKeysFromTfPlan([]byte(tfplanCase))
+	sKeys := GetSensitiveKeysFromTfPlan([]byte(tfplanCase01))
 	addr := attrs["address"].(string)
 	result := SensitiveAttrs(attrs, sKeys[addr], "")
 	if result["name"] != "(sensitive value)" {
@@ -192,7 +192,7 @@ func TestSensitiveAttrs01(t *testing.T) {
 	}
 }
 
-func TestSensitiveAttrs02(t *testing.T) {
+func TestCase01SensitiveAttrs02(t *testing.T) {
 	// external 是敏感变量，但是有前缀 ports
 	var attrs = map[string]interface{}{
 		"external": "external_value",
@@ -200,7 +200,7 @@ func TestSensitiveAttrs02(t *testing.T) {
 		"address":  "module.m.module.m2.docker_container.nginx",
 	}
 
-	sKeys := GetSensitiveKeysFromTfPlan([]byte(tfplanCase))
+	sKeys := GetSensitiveKeysFromTfPlan([]byte(tfplanCase01))
 	addr := attrs["address"].(string)
 	result := SensitiveAttrs(attrs, sKeys[addr], "ports")
 	if result["external"] != "(sensitive value)" {
@@ -212,7 +212,7 @@ func TestSensitiveAttrs02(t *testing.T) {
 	}
 }
 
-func TestSensitiveAttrs03(t *testing.T) {
+func TestCase01SensitiveAttrs03(t *testing.T) {
 	// name 和 external 是敏感变量，但是资源 address 不匹配，所以不加密
 	var attrs = map[string]interface{}{
 		"name":     "name_value",
@@ -220,7 +220,7 @@ func TestSensitiveAttrs03(t *testing.T) {
 		"address":  "address_value",
 	}
 
-	sKeys := GetSensitiveKeysFromTfPlan([]byte(tfplanCase))
+	sKeys := GetSensitiveKeysFromTfPlan([]byte(tfplanCase01))
 	addr := attrs["address"].(string)
 	result := SensitiveAttrs(attrs, sKeys[addr], "")
 	if result["name"] != "name_value" {
@@ -228,5 +228,166 @@ func TestSensitiveAttrs03(t *testing.T) {
 	}
 	if result["external"] != "external_value" {
 		t.Errorf("external attr error: %v\n", result["external"])
+	}
+}
+
+var tfplanCase02 = `{
+	"format_version": "1.1",
+	"terraform_version": "1.2.2",
+	"variables": {
+	  "ami_id": {
+		"value": "ami_iiid"
+	  },
+	  "filename": {
+		"value": "aaaaa"
+	  }
+	},
+	"configuration": {
+	  "provider_config": {
+		"docker": {
+		  "name": "docker",
+		  "full_name": "registry.terraform.io/hashicorp/random",
+		  "version_constraint": "~> 3.3.2"
+		},
+		"module.none:local": {
+		  "name": "local",
+		  "full_name": "registry.terraform.io/hashicorp/local",
+		  "version_constraint": "2.2.3",
+		  "module_address": "module.none"
+		},
+		"random": {
+		  "name": "random",
+		  "full_name": "registry.terraform.io/hashicorp/random"
+		}
+	  },
+	  "root_module": {
+		"resources": [
+		  {
+			"address": "random_id.server",
+			"mode": "managed",
+			"type": "random_id",
+			"name": "server",
+			"provider_config_key": "random",
+			"expressions": {
+			  "byte_length": {
+				"constant_value": 8
+			  },
+			  "keepers": {
+				"references": ["var.ami_id"]
+			  }
+			},
+			"schema_version": 0
+		  }
+		],
+		"module_calls": {
+		  "none": {
+			"source": "./m",
+			"expressions": {
+			  "filename": {
+				"references": ["var.filename"]
+			  }
+			},
+			"module": {
+			  "resources": [
+				{
+				  "address": "local_file.literature",
+				  "mode": "managed",
+				  "type": "local_file",
+				  "name": "literature",
+				  "provider_config_key": "module.none:local",
+				  "expressions": {
+					"content": {
+					  "constant_value": "Sun Tzu said: The art of war is of vital importance to the State.\nIt is a matter of life and death, a road either to safety or to\nruin. Hence it is a subject of inquiry which can on no account be\nneglected....\n"
+					},
+					"filename": {
+					  "references": ["var.filename"]
+					}
+				  },
+				  "schema_version": 0
+				}
+			  ],
+			  "variables": {
+				"filename": {
+				  "default": "bbbb"
+				}
+			  }
+			}
+		  }
+		},
+		"variables": {
+		  "ami_id": {
+			"default": "ami_iiid",
+			"description": "Value of the name for the Docker container",
+			"sensitive": true
+		  },
+		  "filename": {
+			"default": "aaaaa",
+			"sensitive": true
+		  }
+		}
+	  }
+	}
+  }
+`
+
+func TestGetSensitiveKeysFromTfPlanCase02(t *testing.T) {
+	result := GetSensitiveKeysFromTfPlan([]byte(tfplanCase02))
+
+	if result == nil {
+		t.Error("解析失败")
+	}
+
+	t.Logf("result: %v\n", result)
+
+	if _, ok := result["module.none.local_file.literature"]; !ok {
+		t.Error("敏感变量查找失败")
+	}
+
+	if _, ok := result["random_id.server"]; !ok {
+		t.Error("敏感变量查找失败")
+	}
+
+	if len(result) != 2 {
+		t.Error("敏感变量数量错误")
+	}
+}
+
+func TestCase02SensitiveAttrs01(t *testing.T) {
+	// name 是敏感变量
+	var attrs = map[string]interface{}{
+		"filename": "name_value",
+		"keepers":  "keepers_value",
+		"address":  "module.none.local_file.literature",
+	}
+
+	sKeys := GetSensitiveKeysFromTfPlan([]byte(tfplanCase02))
+	addr := attrs["address"].(string)
+	result := SensitiveAttrs(attrs, sKeys[addr], "")
+	if result["filename"] != "(sensitive value)" {
+		t.Errorf("filename attr error: %v\n", result["filename"])
+	}
+
+	if result["keepers"] != "keepers_value" {
+		t.Errorf("keepers attr error: %v\n", result["keepers"])
+	}
+}
+
+func TestCase02SensitiveAttrs02(t *testing.T) {
+	// name 是敏感变量
+	var attrs = map[string]interface{}{
+		"filename": "name_value",
+		"keepers":  "keepers_value",
+		"address":  "random_id.server",
+	}
+
+	sKeys := GetSensitiveKeysFromTfPlan([]byte(tfplanCase02))
+	addr := attrs["address"].(string)
+	result := SensitiveAttrs(attrs, sKeys[addr], "")
+	if result["filename"] != "name_value" {
+		t.Errorf("filename attr error: %v\n", result["filename"])
+	}
+
+	if result["keepers"] != "(sensitive value)" {
+		t.Errorf("keepers attr error: %v\n", result["keepers"])
 	}
 }
