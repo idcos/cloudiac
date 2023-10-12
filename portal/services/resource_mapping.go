3c9e506bcd541b40a17c8533c2324452afe98cdc
jiangliuhong <ja_rome@163.com>
2023-10-12 23:38:53 +0800

    查询条件去重

--- portal/services/resource_mapping.go
+++ portal/services/resource_mapping.go
@@ -9,8 +9,23 @@ import (
 
 // SearchResourceMappingExpress 查询映射表达式
 func SearchResourceMappingExpress(tx *db.Session, rmc []*models.ResourceMappingCondition) (map[string]string, e.Error) {
-	query := tx
+	if len(rmc) == 0 {
+		return map[string]string{}, nil
+	}
+	// 条件去重
+	rmcMap := map[string]bool{}
+	newRmc := make([]*models.ResourceMappingCondition, 0)
 	for _, item := range rmc {
+		key := buildResourceMappingMapKey(item.Provider, item.Type, item.Code)
+		if _, ok := rmcMap[key]; ok {
+			continue
+		} else {
+			rmcMap[key] = true
+			newRmc = append(newRmc, item)
+		}
+	}
+	query := tx.Model(&models.ResourceMapping{})
+	for _, item := range newRmc {
 		query = query.Or("provider = ? and type = ? and code = ?", item.Provider, item.Type, item.Code)
 	}
 	rms := make([]*models.ResourceMapping, 0)
